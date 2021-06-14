1af477c179f2a2c9d433fa0fe0dce7551d9b8d68
aaqib qadeer <aaqibqs@gmail.com>
2021-06-14 17:13:11 +0500

    indent product component

--- packages/shopping-cart/src/components/Product.jsx
+++ packages/shopping-cart/src/components/Product.jsx
@@ -20,13 +20,17 @@ export function Product(props) {
 
   return(
     <div className="card col-3">
-      <img src={props.product.imgUrl} style={{width: "12rem"}} className="mx-auto d-block card-img-top" alt="..."/>
+      <img src={props.product.imgUrl} style={{width: "12rem"}}
+          className="mx-auto d-block card-img-top" alt="..."/>
+          
       <div className="card-body">
         <h5 className="card-title" style={{"minHeight": "3rem"}}> {props.product.name} </h5>
-        {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+        {/* <p className="card-text">Some quick example text.</p> */}
         <p className="card-text fw-bold"> Price: ${props.product.price} </p>
-        { props.authStatus && !isAddedToCart && <button onClick={handleUpdate} className="btn btn-sm btn-success mx-auto d-block">Add to cart</button>}
-        { props.authStatus && isAddedToCart &&  <button onClick={handleRemove} className="btn btn-sm btn-success mx-auto d-block">Remove from cart</button>}
+        { props.authStatus && !isAddedToCart && <button onClick={handleUpdate}
+            className="btn btn-sm btn-success mx-auto d-block">Add to cart</button>}
+        { props.authStatus && isAddedToCart &&  <button onClick={handleRemove}
+            className="btn btn-sm btn-success mx-auto d-block">Remove from cart</button>}
       </div>
     </div>
   )
