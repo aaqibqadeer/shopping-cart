4e112b7de1aa4ddc2da5e5a26f73b203dc79b3a1
aaqib qadeer <aaqibqs@gmail.com>
2021-06-29 04:19:06 +0500

    Remove unnecessary code from app component

--- packages/shopping-cart/src/App.jsx
+++ packages/shopping-cart/src/App.jsx
@@ -14,21 +14,17 @@ import {
 import { Overlay } from "./AppStyle.jsx";
 
 export const AuthContext = React.createContext({});
-export const UsersContext = React.createContext({});
 export const LoadingContext = React.createContext({});
 export const CartContext = React.createContext({});
 
 export const App = () => {
   const [authStatus, setAuthStatus] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [users, setUsers] = useState([
-    { name: "aaqib", email: "aaqib@gmail.com", password: "abc12345" },
+  const [cart, setCart] = useState([
+    // { _id: "60d9ed3dddbeeb63b0bad829", quantity: 1 },
   ]);
-  const [cart, setCart] = useState([{ id: 1, quantity: 1 }]);
 
   const updateStatus = (status) => setAuthStatus(status);
-
-  const addUser = (user) => setUsers([...users, user]);
 
   const updateLoading = (status) => setIsLoading(status);
 
@@ -47,31 +43,29 @@ export const App = () => {
       )}
       <CartContext.Provider value={{ cart, addToCart, updateCart }}>
         <LoadingContext.Provider value={{ isLoading, updateLoading }}>
-          <UsersContext.Provider value={{ users, addUser }}>
-            <AuthContext.Provider value={{ authStatus, updateStatus }}>
-              <Router>
-                <Header />
-                <Switch>
-                  <Route path="/" exact component={Home} />
-                  <Route path="/products" component={Products} />
-                  <Route path="/about" component={About} />
-                  <Route path="/login" component={Login} />
-                  <Route path="/register" component={Register} />
-                  <Route path="/cart" component={Cart} />
-                  <RouteGuard
-                    path="/checkout"
-                    Component={Checkout}
-                    auth={authStatus}
-                  />
-                  <RouteGuard
-                    path="/order-success"
-                    Component={OrderSuccess}
-                    auth={authStatus}
-                  />
-                </Switch>
-              </Router>
-            </AuthContext.Provider>
-          </UsersContext.Provider>
+          <AuthContext.Provider value={{ authStatus, updateStatus }}>
+            <Router>
+              <Header />
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/products" component={Products} />
+                <Route path="/about" component={About} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route path="/cart" component={Cart} />
+                <RouteGuard
+                  path="/checkout"
+                  Component={Checkout}
+                  auth={authStatus}
+                />
+                <RouteGuard
+                  path="/order-success"
+                  Component={OrderSuccess}
+                  auth={authStatus}
+                />
+              </Switch>
+            </Router>
+          </AuthContext.Provider>
         </LoadingContext.Provider>
       </CartContext.Provider>
     </div>
