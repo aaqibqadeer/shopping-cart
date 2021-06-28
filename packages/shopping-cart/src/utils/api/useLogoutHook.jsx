890c56ddf3bdb7d6f8ee792626e3f0bc1fe8a162
aaqib qadeer <aaqibqs@gmail.com>
2021-06-29 04:12:12 +0500

    Add Logout Hook

---
+++ packages/shopping-cart/src/utils/api/useLogoutHook.jsx
@@ -0,0 +1,25 @@
+import { useCallback, useState } from "react";
+import { api } from "./api";
+
+export const useLogoutHook = () => {
+  const [res, setRes] = useState({
+    success: false,
+    loading: false,
+    status: null,
+  });
+
+  const logout = useCallback(async () => {
+    setRes({ success: false, loading: true, status: null });
+    try {
+      const response = await api.delete("/user/logout");
+      setRes({ success: true, loading: false, status: 200 });
+    } catch (error) {
+      setRes({ success: false, loading: false, status: error.response.status });
+    }
+  }, []);
+
+  return {
+    res,
+    logout,
+  };
+};
