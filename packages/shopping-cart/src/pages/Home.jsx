ff5bf9af5b039bfa97a139dd5f67fc41ee47c54e
aaqib qadeer <aaqibqs@gmail.com>
2021-06-29 04:28:02 +0500

    Update Home component

--- packages/shopping-cart/src/pages/Home.jsx
+++ packages/shopping-cart/src/pages/Home.jsx
@@ -1,22 +1,38 @@
-import { useContext } from "react";
-import { AuthContext } from "../App";
-import productsList from "../products.json";
-import { Product } from "../components";
+import { useContext, useEffect } from "react";
+import { AuthContext, LoadingContext } from "../App";
+import { Product, NoProduct } from "../components";
+import { useProductsHook } from "../utils/api";
 
 export const Home = () => {
   const { authStatus } = useContext(AuthContext);
+  const { updateLoading } = useContext(LoadingContext);
+  const { res, getProducts, products } = useProductsHook();
+
+  useEffect(() => {
+    updateLoading(res.loading);
+  }, [res, updateLoading]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const ProductList = () =>
+    products
+      ? products.map((product) => (
+          <Product
+            product={product}
+            key={product._id}
+            id={product._id}
+            authStatus={authStatus}
+          />
+        ))
+      : null;
 
   return (
     <div className="container">
       <div className="row scroll">
-        {productsList.arrayOfProducts.map((product) => (
-          <Product
-            product={product}
-            key={product.id}
-            id={product.id}
-            authStatus={authStatus}
-          />
-        ))}
+        {products.length > 0 && <ProductList />}
+        {products.length === 0 && <NoProduct />}
       </div>
     </div>
   );
--- packages/shopping-cart/src/utils/api/index.js
+++ packages/shopping-cart/src/utils/api/index.js
@@ -2,5 +2,5 @@ export * from "./api";
 export * from "./useLoginHook";
 export * from "./useRegisterHook";
 export * from "./useLogoutHook";
-export * from "./useProductHook";
+export * from "./useProductsHook";
 export * from "./useOrderHook";
